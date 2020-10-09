d3d92a4f78f77b05fff6de0e0c185d9cfa308164
lzf-lamer <firelzf@gmail.com>
2020-10-09 14:33:23 +0800

    😯

---
+++ cmake-build-debug/CMakeFiles/Logger.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Logger.dir/test.cpp.o"
+  "CMakeFiles/Logger.dir/log.cpp.o"
+  "Logger.pdb"
+  "Logger"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Logger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
